a0bb8c64da575c2f971a948f45aaba0045f22206
Amul <amulbashyal6@gmail.com>
2021-12-03 15:34:49 +0545

    student apple ques

---
+++ day 2/st_apple.py
@@ -0,0 +1,6 @@
+student=int(input("enter number of students"))
+apple=int(input("enter number of apples"))
+get=apple//student
+basket=apple%student
+print("number of apple each get = ",get)
+print("remaining on basket= ",basket)
