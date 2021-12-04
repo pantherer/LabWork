88ee4bf867999240bb63080296fba544f7aad8d4
Amul <amulbashyal6@gmail.com>
2021-12-04 11:20:27 +0545

    testing

--- pythonProject/BMI .py
+++
@@ -1,5 +0,0 @@
-mass=float(input("enter mass in kg"))
-height=float(input("enter height in m"))
-BMI=float(mass/height**2)
-print(f"BMI value is {BMI}")
-
---
+++ pythonProject/area_t.py
@@ -0,0 +1,4 @@
+breadth=5
+height=6
+area=1/2*(breadth*height)
+print("area of triangle is ",area)
